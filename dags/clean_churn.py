a1515fab15711dfed1aad4947e40b8d783accd5f
MrKostas <storozhukc@yandex.ru>
2024-08-15 12:23:55 +0000

    clean_churn has been modified

--- dags/clean_churn.py
+++ dags/clean_churn.py
@@ -19,7 +19,7 @@ def clean_churn_dataset():
         from sqlalchemy import MetaData, Table, Column, Integer, Float, String, DateTime, UniqueConstraint, inspect
         
         hook = PostgresHook('destination_db')
-        db_conn = hook.get_sqlalchemy_engine()
+        db_engine = hook.get_sqlalchemy_engine()
 
         metadata = MetaData()
         churn_table = Table(
@@ -49,8 +49,8 @@ def clean_churn_dataset():
             Column("target", Integer),
             UniqueConstraint('customer_id', name = 'unique_clean_customer_constraint'))
         
-        if not sqlalchemy.inspect(db_conn).has_table(churn_table.name):
-            metadata.create_all(db_conn)
+        if not sqlalchemy.inspect(db_engine).has_table(churn_table.name):
+            metadata.create_all(db_engine)
             
     @task()
     def extract():
@@ -64,25 +64,24 @@ def clean_churn_dataset():
         conn.close()
         return data
 
-    @task()
-    def transform(data: pd.DataFrame):
-        data['target'] = (data['end_date'] != 'No').astype(int)
-        data['end_date'].replace({'No': None}, inplace=True)
-        def remove_duplicates(data: pd.DataFrame):
-            feature_cols = data.columns.drop('customer_id').tolist()
-            is_duplicated_features = data.duplicated(subset = feature_cols, keep = False)
-            data = data[~is_duplicated_features].reset_index(drop = True)
-            return data
-        def fill_missing_values(data: pd.DataFrame):
-            cols_with_nans = data.isnull().sum()
-            cols_with_nans = cols_with_nans[cols_with_nans > 0].index.drop('end_date')
-            for col in cols_with_nans:
-                if data[col].dtype in [float, int]:
-                    fill_value = data[col].mean()
-                elif data[col].dtype == 'object':
-                    fill_value = data[col].mode.iloc[0]
-                data[col] = data[col].fillna(fill_value)
-            return data
+    def remove_duplicates(data):
+        feature_cols = data.columns.drop('customer_id').tolist()
+        is_duplicated_features = data.duplicated(subset = feature_cols, keep = False)
+        data = data[~is_duplicated_features].reset_index(drop = True)
+        return data
+        
+    def fill_missing_values(data):
+        cols_with_nans = data.isnull().sum()
+        cols_with_nans = cols_with_nans[cols_with_nans > 0].index.drop('end_date')
+        for col in cols_with_nans:
+            if data[col].dtype in [float, int]:
+                fill_value = data[col].mean()
+            elif data[col].dtype == 'object':
+                fill_value = data[col].mode.iloc[0]
+            data[col] = data[col].fillna(fill_value)
+        return data
+        
+    def remove_errors_records(data):
         num_cols = data.select_dtypes(['float']).columns
         threshold = 1.5
         potential_outliers = pd.DataFrame()
@@ -96,7 +95,15 @@ def clean_churn_dataset():
             upper = Q3 + margin
             potential_outliers[col] = ~data[col].between(lower, upper)
         
-        data = data.drop(potential_outliers.column.tolist(), axis = 1)
+        outliers = potential_outliers.any(axis = 1)
+        data = data[~outliers].reset_index(drop = True)
+        return data
+    
+    @task()
+    def transform(data: pd.DataFrame):
+        data = remove_duplicates(data)
+        data = fill_missing_values(data)
+        data = remove_errors_records(data)
         return data
 
     @task()
